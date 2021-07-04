225c789b16a995d79ab9714f14e5f4e4ea37b069
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-07-04 12:39:40 +0530

    assign-1

--- block-BNaabv/server.js
+++ block-BNaabv/server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(cookieParser());
+console.log();
 
 app.use((req, res, next) => {
   console.log(req.cookies);
